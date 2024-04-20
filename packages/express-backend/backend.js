a746b540177bcc91423ffca905be7b430b8779dc
chris <chmontie@calpoly.edu>
2024-04-20 16:29:36 -0700

    implemented step 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,6 +34,22 @@ const users = {
 
 app.use(express.json());
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+  app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
 
 app.listen(port, () => {
     console.log(
@@ -41,6 +57,3 @@ app.listen(port, () => {
     );
 });
 
-app.get("/users", (req, res) => {
-    res.send(users);
-});
