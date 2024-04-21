1c3b1290e836df3fba442776b07455aacfb31ec4
chris <chmontie@calpoly.edu>
2024-04-20 17:56:07 -0700

    added step 7

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,8 +1,11 @@
+// Import necessary modules
 import express from "express";
 
+// Create Express app and define port
 const app = express();
 const port = 8000;
 
+// Define the list of users
 const users = {
     users_list: [
         {
@@ -10,7 +13,8 @@ const users = {
             name: "Charlie",
             job: "Janitor"
         },
-        {    id: "abc123",
+        {    
+            id: "abc123",
             name: "Mac",
             job: "Bouncer"
         },
@@ -32,45 +36,59 @@ const users = {
     ]
 };
 
+// Middleware to parse JSON requests
 app.use(express.json());
 
+// Function to find a user by their name
 const findUserByName = (name) => {
     return users["users_list"].filter(
-      (user) => user["name"] === name
+        (user) => user["name"] === name
     );
-  };
-  
-  const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
+};
 
+// Function to find a user by their ID
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
 
+// Endpoint to retrieve a user by their ID
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+    const id = req.params["id"];
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
 });
 
+// Endpoint to add a new user
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
 
+// Function to add a user to the list
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+// Endpoint to delete a user by their ID
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    const index = users["users_list"].findIndex(user => user.id === id);
+    if (index !== -1) {
+        users["users_list"].splice(index, 1);
+        res.status(204).send(); // No content response
+    } else {
+        res.status(404).send("Resource not found.");
+    }
+});
+
+// Start the server
 app.listen(port, () => {
     console.log(
         `Example app listening at http://localhost:${port}`
     );
 });
-
-
-const addUser = (user) => {
-    users["users_list"].push(user);
-    return user;
-  };
-  
-  app.post("/users", (req, res) => {
-    const userToAdd = req.body;
-    addUser(userToAdd);
-    res.send();
-  });
-
-
