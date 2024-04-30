a8a9ee8235f3efe9b379b1d42ec5769191de8c9e
chris <chmontie@calpoly.edu>
2024-04-30 11:48:46 -0700

    added everything

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,82 +1,96 @@
-import React, { useState, useEffect } from 'react';
+// src/MyApp.jsx 
+import React, { useState, useEffect } from "react";  
 import Table from "./Table";
 import Form from "./Form";
 
-function MyApp() {
-  const [characters, setCharacters] = useState([]);
+function MyApp() {   
+    const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(id) {
-    // Make HTTP DELETE request to delete user by ID
-    fetch(`http://localhost:8000/users/${id}`, {
-      method: 'DELETE'
-    })
-    .then((response) => {
-      if (response.status === 204) {
-        // If successful deletion, update frontend state
-        setCharacters(characters.filter((character) => character._id !== id)); // Change 'id' to '_id'
-      } else if (response.status === 404) {
-        // If user not found, show error message or handle it appropriately
-        console.log("User not found.");
-      } else {
-        // Handle other error cases if needed
-        console.log("Failed to delete user.");
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }
+    function removeOneCharacter(index) {     
+	const characterToDelete = characters[index];
+	fetch(`http://localhost:8000/users/${characterToDelete._id}`, {
+	    method: "DELETE",
+	    headers: {
+		"Content-Type": "application/json",
+	    },
+	})
+	    .then((response) => {
+		if(reponse.status === 204)
+		{
+		    const updated = characters.filter((_, i) => i !== index);
+		    setCharacters(updated);
+		}
+		else if(response.status === 404)
+		{
+		    alert("User not found.");
+		}
+		else
+		{
+		    throw new Error(`Request failed with status ${response.status}`);
+		}
+	    })
+	    .catch((error) => {
+		console.error(error);
+	    }); 
+    }
 
-  function updateList(person) { 
-    postUser(person)
-      .then((response) => {
-        if (response.status === 201) {
-          return response.json();
-        } else {
-          throw new Error("Failed to add user");
-        }
-      })
-      .then((data) => {
-        setCharacters([...characters, data]);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-  
-  function fetchUsers() {
-    const promise = fetch("http://localhost:8000/users");
-    return promise;
-  }
+    function updateList(person) {
+	postUser(person)
+	    .then(() => setCharacters([...characters, person]))
+	    .catch((error) => {
+		console.log(error);
+	    })
+    }
 
-  useEffect(() => {
-    fetchUsers()
-      .then((res) => res.json())
-      .then((json) => setCharacters(json["users_list"]))
-      .catch((error) => { console.log(error); });
-  }, [] );
+    function fetchUsers() {
+	const promise = fetch("http://localhost:8000/users");
+	return promise;
+    }
 
-  function postUser(person) {
-    const promise = fetch("http://localhost:8000/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(person),
-    });
+    useEffect(() => {
+	fetchUsers()
+	   .then((res) => res.json())
+	   .then((json) => setCharacters(json))
+	   .catch((error) => { console.log(error); });
+    }, [] );
 
-    return promise;
-  }
+    function postUser(person) {
+	const promise = fetch("http://localhost:8000/users", {
+	    method: "POST",
+	    headers: {
+		"Content-Type": "application/json",
+	    },
+	    body: JSON.stringify(person),
+	})
+	    .then((response) => {
+		if(response.status === 201) // get the http status 
+		{
+		    return response.json(); // parse as json
+		}
+		else
+		{
+		    throw new Error(`Request failed with status ${response.status}`);
+		}
+	    })
+	    .then((data) => {
+		setCharacters([...characters, data]);
+	    })
+	    .catch((error) => {
+		console.error(error);
+	    });
 
-  return (
-    <div className="container">
-      <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-      />
-      <Form handleSubmit={updateList} />
-    </div>
-  );
-}
+	return promise;
+    }
+
+    return (     
+        <div className="container">       
+	    <Table
+	        characterData={characters} 
+	        removeCharacter={removeOneCharacter}
+	    />
+	    <Form handleSubmit={updateList} />
+        </div>   
+    );
+} 
 
 export default MyApp;
